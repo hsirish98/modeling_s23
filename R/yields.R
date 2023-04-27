6b23ee860a249c6df1ee2b22ea6c04acfc5bb58f
hsirish98 <hannahsirish@gmail.com>
2023-04-27 10:25:20 -0700

    after resubmitting to canvas

--- R/yields.R
+++ R/yields.R
@@ -2,103 +2,45 @@
 #'
 #' calculating the min, max and mean anomaly almond yield
 #' This function computes the min, max, and mean anomalies of almond yield for months from 1988 to 2010
-#' @param dataset is a time series data set conatining information on day, mont, year, and water year of data collection for max temperature in celcius, min temperature in celcius and precipitation
+#' @param dataset is a time series data set containing information on day, month, year, and water year of data collection for max temperature in Celsius, min temperature in Celsius and precipitation
+#' @param P1 is the first constant in the almond yield equation
+#' @param P2 is the second constant in the almond yield equation
+#' @param P3 is the third constant in the almond yield equation
+#' @param P4 is the third constant in the almond yield equation
+#' @param P5 is the third constant in the almond yield equation
 #' @examples yields(climate_data)
 #' @author Hannah Irish % Nadine Snyder
 
 
 
-yields <- function(dataset){
+yields <- function(dataset, P1=0.015, P2=0.0046, P3=0.07, P4=0.0043, P5=0.28){
 
   
-  ##group the data set by year and month to generate the precipitation sum
-  ##and the min temp for each month
+  temp_feb <- as.data.frame(data) %>%
+    filter(month==2) %>%
+    group_by(year) %>%
+    summarise(min_temp = min(tmin_c))
   
-  data_test <- as.data.frame(dataset) %>%
-    group_by(year, month) %>%
-    mutate(min_temp = min(tmin_c), sum_precip = sum(precip)) %>%
-    ungroup()
+  precip_jan <- as.data.frame(data) %>%
+    filter(month==1) %>%
+    group_by(year) %>%
+    summarise(precip_sum = sum(precip))
   
   
-  ##make function that uses the formula included in the paper
-  calculate_yield <- function(temp, p) {
-    yield = (-0.015*temp) - (0.0046*(temp^2)) -(0.07*p) + (0.0043*(p^2)) + 0.28
-    
+  combine <- cbind(temp_feb, precip_jan)
+  
+  
+  calculate_yield <- function(temp, p, var1=P1, var2=P2, var3=P3, var4=P4, var5=P5) {
+    yield = -(var1*temp) - (var2*(temp^2)) -(var3*p) + (var4*p^2) + var5
     return(yield)
   }
   
-  ##make empty vector
-  vec_test <- c()
-  
-  ##and empty matrix
-  yield_m <- matrix()
-  
-  ##initialize rhe year to 1988
-  year_iter = 1988
-  
-  ##initialize a counter for while loop
-  j=1
-  
-  ##while loop until year is past 2010
-  while (year_iter <2011){
-    
-    ##filter data set by given year
-    by_year <- data_test %>%
-      filter(year==year_iter)
-    
-    ##for loop to go through each month
-    for(i in 1:12){
-     
-      ##filter data set by month 
-      month <- by_year %>%
-        filter(month==i)
-      
-      ##select temperature variable which is the min
-      temps <- month %>%
-        select(min_temp)
-      
-      ##get any of them because they're all the min
-      temp <- as.numeric(temps[1,1])
-      
-      ##select precipitation variable which is the sum for the month
-      precipitations <- month %>%
-        select(sum_precip)
-      
-      ##get any which is the sum
-      precip_sum <- as.numeric(precipitations[1,1])
-      
-      ##If neither temp nor precipitation is NA (got a value)
-      if(is.na(temp)== FALSE)
-      {
-        if(is.na(precip_sum)==FALSE)
-        {
-          ##then calculate value for yield
-          addition = calculate_yield(temp, precip_sum)
-        }
-      } else( ##otherwise give NA
-        addition = NA
-      )
-      
-      ##add whatever "addition" is (value or NA) to vector 
-      vec_test[i]=addition
-      
-    }
-    
-    ##add vector to matrix
-    yield_m[j] = vec_test
-    
-    ##update year to year later
-    year_iter = year_iter + 1
-    
-    ##update counter which gives column in matrix
-    j=j+1
-    
-  }
+  vector <- mapply(calculate_yield, temp=combine$min_temp, p=combine$precip_sum)
   
   ##calculate min, max, and mean with no na's
-  min <- min(yield_m, na.rm=TRUE)
-  max <- max(yield_m, na.rm=TRUE)
-  mean <- mean(yield_m, na.rm=TRUE)
+  min <- min(vector, na.rm=TRUE)
+  max <- max(vector, na.rm=TRUE)
+  mean <- mean(vector, na.rm=TRUE)
   
   ##return list of these three
   return(list(min,max,mean))
